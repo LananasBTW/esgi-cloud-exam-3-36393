11f132c043fd8b97007a789e92f6fefc47497b21
Abane Zreik <abane.zreik@gmail.com>
2025-05-07 15:26:22 +0200

    Aled

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: 'C5Y88EE1BIMS08LO37CL',        
+    secretAccessKey: 'fEDkR6zGruhPYob9NzFaUbnZupYRILo8ezuwYj3e',
+    endpoint: 's3.eu-west-2.wasabisys.com',    
+    bucket: 'esgi-cloud-exam-bucket-36393',    
   },
   suffix: {
     small: '_small.jpg',
